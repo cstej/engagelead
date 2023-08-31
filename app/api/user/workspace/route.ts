57d559367a04875c02125606cefd8daa99d74d22
Tejendra Kumar <cstejendra@gmail.com>
2023-08-31 20:12:32 +0530

    tried to fix middleware issue

--- app/api/user/workspace/route.ts
+++ app/api/user/workspace/route.ts
@@ -1,30 +1,49 @@
-import { NextRequest, NextResponse } from "next/server"
-
-import { prisma } from "@/lib/prisma"
-import { getCurrentUser, getSession } from "@/lib/sessions"
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { getCurrentUser } from "@/lib/sessions";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const user = await getCurrentUser()
-  if (!user) return null
+  try {
+    const user = await getCurrentUser();
+    if (!user) {
+      return new NextResponse(null, {
+        status: 401,
+        statusText: "Unauthorized",
+      });
+    }
 
-  const workspace = await prisma.workspaceMember.findFirst({
-    where: {
-      userId: user.id,
-    },
-    select: {
-      workspace: {
-        select: {
-          id: true,
-          name: true,
+    const workspace = await prisma.workspaceMember.findFirst({
+      where: {
+        userId: user.id,
+      },
+      select: {
+        workspace: {
+          select: {
+            id: true,
+            name: true,
+          },
         },
       },
-    },
-    orderBy: {
-      workspace: {
-        createdAt: "asc",
+      orderBy: {
+        workspace: {
+          createdAt: "asc",
+        },
       },
-    },
-  })
+    });
 
-  return NextResponse.json(workspace?.workspace || null)
+    if (!workspace) {
+      return new NextResponse(null, {
+        status: 404,
+        statusText: "Not Found",
+      });
+    }
+
+    return NextResponse.json(workspace.workspace);
+  } catch (error) {
+    console.error("Error getting workspace:", error);
+    return new NextResponse(null, {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,75 +1,100 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-import { withAuth } from "next-auth/middleware";
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
+import { withAuth } from "next-auth/middleware"
 
-export default withAuth(async function middleware(req) {
-  const isDashboardPage = req.nextUrl.pathname.startsWith("/dashboard");
-  const token = await getToken({ req });
-  const isAuth = !!token;
-  const isAuthPage = req.nextUrl.pathname.startsWith("/login") || req.nextUrl.pathname.startsWith("/signup");
+export default withAuth(
+  async function middleware(req) {
+    try {
+      const isDashboardPage = req.nextUrl.pathname.startsWith("/dashboard")
+      const token = await getToken({ req })
+      const isAuth = !!token
+      const isAuthPage =
+        req.nextUrl.pathname.startsWith("/login") ||
+        req.nextUrl.pathname.startsWith("/signup")
 
-  if (isAuthPage) {
-    if (isAuth) {
-      // Redirect authenticated users to the dashboard.
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
+      if (isAuthPage) {
+        if (isAuth) {
+          // Redirect authenticated users to the dashboard.
+          return NextResponse.redirect(new URL("/dashboard", req.url))
+        }
 
-    // If it's an authentication page and the user is not authenticated, allow access.
-    return null;
-  }
+        // If it's an authentication page and the user is not authenticated, allow access.
+        return null
+      }
 
-  if (!isAuth) {
-    // If the user is not authenticated, redirect them to the login page with the 'from' parameter.
-    let from = req.nextUrl.pathname;
-    if (req.nextUrl.search) {
-      from += req.nextUrl.search;
-    }
+      if (!isAuth) {
+        // If the user is not authenticated, redirect them to the login page with the 'from' parameter.
+        let from = req.nextUrl.pathname
+        if (req.nextUrl.search) {
+          from += req.nextUrl.search
+        }
 
-    return NextResponse.redirect(new URL(`/login?from=${encodeURIComponent(from)}`, req.url));
-  }
+        return NextResponse.redirect(
+          new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+        )
+      }
 
-  if (isDashboardPage) {
-    const workspace = cookies().get("workspace")?.value;
+      if (isDashboardPage) {
+        const workspace = cookies().get("workspace")?.value
 
-    if (workspace === "null" || workspace === "{}" || !workspace) {
-      // If workspace information is missing or empty, fetch it.
-      const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/user/workspace`, {
-        method: "GET",
-        cache: "no-store",
-        headers: {
-          "Content-Type": "application/json",
-          Cookie: cookies().toString(),
-        },
-      });
+        if (workspace === "null" || workspace === "{}" || !workspace) {
+          // If workspace information is missing or empty, fetch it.
 
-      const cw = await res.json();
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_APP_URL}/api/user/workspace`,
+            {
+              method: "GET",
+              cache: "no-store",
+              headers: {
+                "Content-Type": "application/json",
+                Cookie: cookies().toString(),
+              },
+            }
+          )
+
+       
+
+          if(res.status === 200) {
+
+           const cw = await res.json()
+
+            if (!cw) {
+              console.log("redirecting to workspace setup")
+              // If workspace information is still missing, redirect to workspace setup.
+              return NextResponse.redirect(new URL("/workspace", req.url))
+            }
   
-      if (!cw) {
-        console.log("redirecting to workspace setup");
-        // If workspace information is still missing, redirect to workspace setup.
-        return NextResponse.redirect(new URL("/workspace", req.url));
-      }
+            if (cw) {
+              console.log("setting workspace cookie")
+              // If workspace information is available, set it as a cookie and continue.
+              const response = NextResponse.redirect(req.url)
+              response.cookies.set("workspace", JSON.stringify(cw))
+              return response
+            }
 
-      if (cw) {
-        console.log("setting workspace cookie");
-        // If workspace information is available, set it as a cookie and continue.
-        const response = NextResponse.redirect(req.url);
-        response.cookies.set("workspace", JSON.stringify(cw));
-        return response;
+          }
+
+        
+
+        
+        }
       }
+    } catch (err) {
+      console.log(err)
     }
-  }
-}, {
-  callbacks: {
-    async authorized({ req, token }) {
-      // This callback can be used for additional authorization checks if needed.
-      return true;
-    },
   },
-});
+  {
+    callbacks: {
+      async authorized({ req, token }) {
+        // This callback can be used for additional authorization checks if needed.
+        return true
+      },
+    },
+  }
+)
 
 export const config = {
   // Define the URL patterns to which this middleware should be applied.
   matcher: ["/dashboard/:path*", "/login", "/workspace", "/accept-invite"],
-};
+}
