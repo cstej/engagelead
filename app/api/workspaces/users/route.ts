ca239524f913a6d10e46e9f510a177fedf0126bc
Tejendra Kumar <cstejendra@gmail.com>
2023-09-24 17:23:47 +0530

    some bugs fixes

--- app/api/workspaces/users/route.ts
+++ app/api/workspaces/users/route.ts
@@ -1,41 +1,52 @@
 // get all users in a workspace
 
-import { NextRequest, NextResponse } from "next/server"
+import { type NextRequest, NextResponse } from "next/server"
 
 import { prisma } from "@/lib/prisma"
 import { getCurrentUserAndWorkspace } from "@/lib/sessions"
 import _ from "lodash"
+import { getErrorMessage } from "@/lib/exceptions/errors"
+import { HttpStatusCode } from "axios"
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest): Promise<NextResponse> {
 
-  const uw = await getCurrentUserAndWorkspace()
+  try {
 
-  if (!uw) {
-    return new NextResponse(null, {
-      status: 401,
-      statusText: "Unauthorized",
+    const uw = await getCurrentUserAndWorkspace()
+
+    if (!uw) {
+     return NextResponse.json({ message: "Unauthorized Request" }, { status: HttpStatusCode.Unauthorized })
+    }
+  
+    const users = await prisma.workspaceMember.findMany({
+      where: {
+          workspaceId: uw.workspaceId
+      },
+      select:{
+          user: {
+              select: {
+                  id: true,
+                  name: true,
+                  email: true,
+                  image: true
+              }
+          }
+      },
+  
     })
+  
+    const userList = users.map((item) => item.user);
+    return NextResponse.json(userList)
+    
+  } catch (error) {
+    
+
+    return NextResponse.json(
+      { message: getErrorMessage(error) },
+      { status: HttpStatusCode.InternalServerError }
+    )
   }
 
-  const users = await prisma.workspaceMember.findMany({
-    where: {
-        workspaceId: uw.workspaceId
-    },
-    select:{
-        user: {
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                image: true
-            }
-        }
-    },
-
-  })
-
-  const userList = users.map((item) => item.user);
 
 
-  return NextResponse.json(userList)
 }
